ab036256237140db769befb7dbb5eff2e6db0e16
Дмитрий Жданов <D.Zhdanov66@ya.ru>
2025-05-23 01:10:28 +0300

    add: основная логика игры

--- main.cpp
+++ main.cpp
@@ -1,23 +1,8 @@
-#include <SFML/Graphics.hpp>
+#include "game.h"
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    sf::Event event;
-    while (window.isOpen())
-    {
-        while (window.pollEvent(event)) 
-        {
-            if (event.type == sf::Event::Closed) 
-                window.close();
-        }
+int main() {
+    Game().run();
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    return 0;
 }
-
